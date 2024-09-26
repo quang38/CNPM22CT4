40b9594ce9496ad101234e84487032bc3b3b9c31
quang38 <122759382+quang38@users.noreply.github.com>
2024-09-26 13:13:25 +0700

    Add files via upload

---
+++ sql and pdf/cnpm22ct4.sql
@@ -0,0 +1,49 @@
+
+use master;
+go
+DROP DATABASE IF EXISTS CNPM22CT4;
+go
+CREATE DATABASE CNPM22CT4;
+GO
+use CNPM22CT4;
+go
+
+DROP TABLE IF EXISTS taikhoan;
+go
+CREATE TABLE taikhoan(
+id_taikhoan INT IDENTITY(1,1) PRIMARY KEY,
+taikhoan VARCHAR(50),
+matkhau VARCHAR(50),
+gmail VARCHAR(50),
+hovaten NVARCHAR(50),
+quyen VARCHAR(10) DEFAULT 'user'
+);
+go
+DROP TABLE IF EXISTS baiviet;
+go
+CREATE TABLE baiviet(
+id_baiviet INT IDENTITY(1,1) PRIMARY KEY,
+id_tacgia INT,
+baiviet NVARCHAR,
+danhmuc NVARCHAR(50),
+danhgia INT,
+tinhtrang NVARCHAR(50),
+binhluan NVARCHAR(1000));
+go
+DROP TABLE IF EXISTS baiviet_chinhsua;
+go
+CREATE TABLE baiviet_chinhsua(
+id_baiviet INT IDENTITY(1,1) PRIMARY KEY,
+id_tacgia INT,
+baiviet NVARCHAR,
+danhmuc NVARCHAR(50),
+danhgia INT,
+tinhtrang NVARCHAR(50),
+binhluan NVARCHAR(1000));
+go
+DROP TABLE IF EXISTS Folow;
+go
+CREATE TABLE Folow(
+id_dangfolow INT,
+id_duocfolow INT);
+go
